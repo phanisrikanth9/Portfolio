1f955438d3eb8a07601d7c77e47e32a84e79d645
phanisrikanth9 <137520011+phanisrikanth9@users.noreply.github.com>
2024-09-17 00:52:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleOptions() {
+    var menu = document.getElementById('options-menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
